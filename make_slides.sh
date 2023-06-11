4c318b13489163309d52e320d54c3e925de6efb5
Tilman Troester <tilman.troester@gmail.com>
2023-06-11 15:50:59 +0200

    add slides script

---
+++ make_slides.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+jupyter nbconvert $1 \
+--to slides \
+--output-dir slides \
+--TemplateExporter.extra_template_basedirs=./nbconvert_templates \
+--TemplateExporter.exclude_input_prompt=True \
+--TemplateExporter.exclude_output_prompt=True \
+--SlidesExporter.mathjax_url=https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js \
+--SlidesExporter.reveal_number="c" \
+--SlidesExporter.reveal_theme="white" \
+--TagRemovePreprocessor.remove_cell_tags="remove-cell" \
+--TagRemovePreprocessor.remove_all_outputs_tags="remove-output" \
+--TagRemovePreprocessor.remove_input_tags="remove-input"
